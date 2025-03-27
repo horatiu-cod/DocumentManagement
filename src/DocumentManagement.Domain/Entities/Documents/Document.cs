fd17970342e48eef80619438b452d1c0446e6ff5
horatiu <53142289+horatiu-cod@users.noreply.github.com>
2025-03-27 18:36:17 +0000

    Initial

--- src/DocumentManagement.Domain/Entities/Documents/Document.cs
+++ src/DocumentManagement.Domain/Entities/Documents/Document.cs
@@ -1,9 +1,9 @@
 
-
+namespace DocumentManagement.Domain.Entities;
     public class Document
     {
         public Guid Key {get; set;}
-        public string FileName {get; set;}
-        public string FileUrl {get; set;}
+        public string FileName {get; set;} = String.Empty;
+        public string FileUrl {get; set;} = String.Empty;
     }
 
